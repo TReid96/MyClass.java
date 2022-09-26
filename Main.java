6593e38e6d8796f550cf701ddc4bd2e247815cd3
TReid96 <temarkusr@gmail.com>
2022-09-26 00:05:01 -0400

    MainJava

---
+++ Main.java
@@ -0,0 +1,30 @@
+package javaVarables;
+
+public class Main {
+
+public static void main(String[] args) {
+	 
+ 	int t = 9;
+	int r = 5;
+	
+	System.out.println(t + r);
+	System.out.println(t - r);
+	System.out.println(t * r);
+	System.out.println(t / r);
+	System.out.println(t % r);
+	System.out.println(t);
+	System.out.println(r);
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+}
